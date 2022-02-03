ade3e72b49b367c5fc75365993b92188912ea599
Jack <jack.rappazzo@gmail.com>
2022-02-02 19:42:19 -0500

    Added input interpreter and tests.

---
+++ CodecRover.Tests/TestInterpreter.cs
@@ -0,0 +1,33 @@
+﻿using CodecRover.Core;
+using CodecRover.Core.Environments;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CodecRover.Tests
+{
+    public class TestInterpreter
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestInput_FFF()
+        {
+            string input = "FFF";
+            List<Command> commands = InputInterpreter.ParseInput(input);
+            Assert.IsTrue(commands.TrueForAll(c => c == Command.Advance));
+        }
+        
+        [Test]
+        public void TestInput_LRF()
+        {
+            string input = "LRF";
+            List<Command> commands = InputInterpreter.ParseInput(input);
+            Assert.IsTrue(commands[0] == Command.TurnLeft &&
+                commands[1] == Command.TurnRight &&
+                commands[2] == Command.Advance);
+        }
+    }
+}
---
+++ CodecRover/InputInterpreter.cs
@@ -0,0 +1,40 @@
+﻿using CodecRover.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodecRover
+{
+    public static class InputInterpreter
+    {
+
+        public static List<Command> ParseInput(string input)
+        {
+            List<Command> commands = new List<Command>();
+            foreach (var c in input.ToLower().ToCharArray())
+            {
+                commands.Add(GetCommand(c));
+            }
+            return commands;
+        }
+
+        static Command GetCommand(char c)
+        {
+            switch (c)
+            {
+                case 'l':
+                    return Command.TurnLeft;
+                    break;
+                case 'r':
+                    return Command.TurnRight;
+                    break;
+                case 'f':
+                    return Command.Advance;
+                    break;
+                default: throw new ArgumentException("Invalid input");
+            }
+        }
+    }
+}
